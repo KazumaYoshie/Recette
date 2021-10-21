f3b70167d22ae24ccb1635d50258204456201b18
KazumaYoshie <yosh1kazu217629@gmail.com>
2021-10-21 07:26:28 +0000

    [Add]follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship",foreign_key: "followed_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :followd
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
   attachment :profile_image
@@ -19,4 +19,15 @@ class User < ApplicationRecord
     super && (self.is_valid == true)
   end
 
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
