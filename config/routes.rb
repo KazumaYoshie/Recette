44f8bfd142938e701d590216ddb722a739f7272a
KazumaYoshie <yosh1kazu217629@gmail.com>
2021-10-22 06:20:38 +0000

    [Add]search

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
     resource :likes, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
   resources :users do
     resource :relationships, only: [:create, :destroy]
