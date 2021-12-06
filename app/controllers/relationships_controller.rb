270ca23361c72487338affa96a891d92ec01e9dc
KazumaYoshie <yosh1kazu217629@gmail.com>
2021-12-06 08:02:26 +0000

    [Add]follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -14,11 +14,13 @@ class RelationshipsController < ApplicationController
   def followings
     user = User.find(params[:user_id])
     @users = user.followings
+    @user = User.find(params[:user_id])
   end
 
   def followers
     user = User.find(params[:user_id])
     @users = user.followers
+    @user = User.find(params[:user_id])
   end
 
 
